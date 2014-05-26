b744ce22970c6ffcf6bdbcfb5143e96c268c5fc0
Dărescu Ionuț <johnnyslt@users.noreply.github.com>
2014-05-26 20:46:49 +0300

    Update flags for toolchains

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,9 @@ COMMON_GLOBAL_CFLAGS += -DCAMERA_WITH_CITYID_PARAM
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 TARGET_KERNEL_CONFIG := liquid_i9305_defconfig
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := sm-arm-eabi-4.7
+
+TARGET_GCC_VERSION_ARM := 4.7-sm
+TARGET_GCC_VERSION_AND := 4.8-sm
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/samsung/i9305/rootdir/fstab.smdk4x12
